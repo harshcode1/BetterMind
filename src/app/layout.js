f1534374580922ce718b47a348f214c261dc7680
ashishsahario04@gmail.com <ashishsahario04@gmail.com>
2025-03-17 11:47:14 +0530

    tailwind is being applied

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import './globals.css';
+import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
 
